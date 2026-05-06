c5d0fe37725f1e9ca804d24cac19bb23815950d3
yashchotala <chotalayash2801@gmail.com>
2026-05-06 17:38:01 +0530

    fixed frontend deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,1 +1,35 @@
-import axios from 'axios';const API = axios.create({ baseURL: 'http://localhost:5000/api' });API.interceptors.request.use((req) => { const token = localStorage.getItem('token'); if (token) req.headers.Authorization = `Bearer ${token}`; return req; });API.interceptors.response.use((res) => res, (err) => { if (err.response?.status === 401) { localStorage.removeItem('token'); window.location.href = '/login'; } return Promise.reject(err); });export default API;
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api`
+});
+
+API.interceptors.request.use((req) => {
+
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return req;
+});
+
+API.interceptors.response.use(
+
+  (res) => res,
+
+  (err) => {
+
+    if (err.response?.status === 401) {
+
+      localStorage.removeItem('token');
+
+      window.location.href = '/login';
+    }
+
+    return Promise.reject(err);
+  }
+);
+
+export default API;
